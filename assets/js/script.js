1cb6d93146bc90dd632bb7dd2e3e142f96545c80
Loris Schlatter <lorisschlatter@gmail.com>
2024-01-19 14:46:59 +0100

    changement nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ window.addEventListener('scroll', function() {
         intro.style.paddingTop = "12%"
     }else{
         header.classList.remove("fixedBar")
-        intro.style.paddingTop = "8%"
     }
 
 });
